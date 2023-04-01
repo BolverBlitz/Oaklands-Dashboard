3195012941dbdcade194b793d2044663eb863821
Marc <bolverblitz@ebg.pw>
2023-04-01 12:01:16 +0200

    RouterOS inferface fixes

--- rostest.js
+++ rostest.js
@@ -2,8 +2,14 @@ const ROSInterface = require('./lib/routerOS');
 
 const ros = new ROSInterface();
 
-ros.getInterfaceList().then((data) => {
-    console.log(data);
+ros.getInterfaceList().then((interfaces) => {
+    const activeEthInterfaces = interfaces.filter((interface) => { return interface.running === 'true' && interface.type === 'ether' });
+    //console.log(activeEthInterfaces);
+    ros.getInterfaceStats(activeEthInterfaces[0].name).then((data) => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(error);
+    });
 }).catch((error) => {
     console.log(error);
 });
